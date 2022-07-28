6048e2948ef0c00258204805ecbc0df8dec46843
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-07-28 17:54:09 -0300

    Added method to ViewModel, TODO: implement throwable

--- app/src/main/java/com/example/marvel/domain/usecase/GetCharacterUseCase.kt
+++ app/src/main/java/com/example/marvel/domain/usecase/GetCharacterUseCase.kt
@@ -1,4 +1,10 @@
 package com.example.marvel.domain.usecase
 
-class GetCharacterUseCase {
+import com.example.marvel.data.repositories.CharacterRespository
+import com.example.marvel.domain.data.Character
+import javax.inject.Inject
+
+class GetCharacterUseCase @Inject constructor(private val repository: CharacterRespository) {
+
+    suspend fun invoke() : List<Character> = repository.getCharacters()
 }
--- app/src/main/java/com/example/marvel/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/marvel/ui/main/viewmodel/MainViewModel.kt
@@ -3,9 +3,11 @@ package com.example.marvel.ui.main.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.marvel.domain.data.Character
 import com.example.marvel.domain.usecase.GetCharacterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,4 +17,14 @@ class MainViewModel @Inject constructor(
 
     private val _characterList = MutableLiveData<List<Character>>()
     val characterList: LiveData<List<Character>> = _characterList
+
+    fun getCharacterList() {
+        viewModelScope.launch {
+            try {
+                _characterList.value = getCharacter.invoke()
+            } catch (throwable: Throwable) {
+                _characterList.value = emptyList()
+            }
+        }
+    }
 }
