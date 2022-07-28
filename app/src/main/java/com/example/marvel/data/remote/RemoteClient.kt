0f1e5c5e3ca8b61a2dcf0fdee8736b321c939e9a
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-07-28 15:21:53 -0300

    Implemented RemoteProvider method

--- app/src/main/java/com/example/marvel/data/remote/RemoteClient.kt
+++ app/src/main/java/com/example/marvel/data/remote/RemoteClient.kt
@@ -2,10 +2,16 @@ package com.example.marvel.data.remote
 
 import com.example.marvel.data.remote.responses.ServerCharacter
 import com.example.marvel.data.repositories.RemoteProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class RemoteClient : RemoteProvider {
+class RemoteClient @Inject constructor(private val api: MarvelAPI) : RemoteProvider {
 
     override suspend fun getCharacters(): List<ServerCharacter> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            val data = api.getCharacters()
+            data.body()?.data?.results ?: emptyList()
+        }
     }
 }
