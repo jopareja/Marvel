8526663066c6f007fd9492663786577201b85a64
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-07-28 16:48:03 -0300

    Added Query to Interface, created fun toMD5

--- app/src/main/java/com/example/marvel/data/remote/MarvelAPI.kt
+++ app/src/main/java/com/example/marvel/data/remote/MarvelAPI.kt
@@ -1,11 +1,17 @@
 package com.example.marvel.data.remote
 
+import com.example.marvel.R
 import com.example.marvel.data.remote.responses.CharacterDataWrapper
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MarvelAPI {
 
     @GET("/v1/public/characters")
-    suspend fun getCharacters(): Response<CharacterDataWrapper>
+    suspend fun getCharacters(
+        @Query("ts") ts: String,
+        @Query("apikey") apikey: String,
+        @Query("hash") hash: String
+    ): Response<CharacterDataWrapper>
 }
--- app/src/main/java/com/example/marvel/data/remote/RemoteClient.kt
+++ app/src/main/java/com/example/marvel/data/remote/RemoteClient.kt
@@ -1,17 +1,30 @@
 package com.example.marvel.data.remote
 
+import com.example.marvel.R
 import com.example.marvel.data.remote.responses.ServerCharacter
 import com.example.marvel.data.repositories.RemoteProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.math.BigInteger
+import java.security.MessageDigest
 import javax.inject.Inject
 
 class RemoteClient @Inject constructor(private val api: MarvelAPI) : RemoteProvider {
 
     override suspend fun getCharacters(): List<ServerCharacter> {
         return withContext(Dispatchers.IO) {
-            val data = api.getCharacters()
+            val data = api.getCharacters("1",
+                R.string.public_key.toString(),
+                toMD5("1", R.string.public_key.toString(), R.string.private_key.toString()))
             data.body()?.data?.results ?: emptyList()
         }
+    }
+
+
+    private fun toMD5(ts: String, publicKey: String, privateKey: String) : String {
+        val input = ts+publicKey+privateKey
+        val md = MessageDigest.getInstance("MD5")
+        val byte = md.digest(input.toByteArray())
+        return BigInteger(1, byte).toString(16).padStart(32, '0')
     }
 }
